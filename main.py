5cf0dc59d60e4961ae41846e4d2529ad56bf4085
Lucie Lénertová <lenertova.lucie@gmail.com>
2022-08-26 18:13:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# nespouštějte program!
+# jaké řetězce se podle vás vytisknou do konzole? a proč?
+# vaši předpověď napište do komentářů níže
+#
+#
+#
+answer = "Berlin"
+
+while answer != "Paris":
+  print("Incorrect! Try again.")
+  answer = "Paris"
+
+print("Correct!")
